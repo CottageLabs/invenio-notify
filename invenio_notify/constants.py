fedcd9c02cffb34b09c7f6964c95192f23f6690b
seabird64 <seabird64@email.not.exit.io>
2025-03-07 12:37:26 +0000

    add constants for Invenio-Notify module

---
+++ invenio_notify/constants.py
@@ -0,0 +1,6 @@
+"""Constants for the Invenio-Notify module."""
+
+# Notification types
+TYPE_REVIEW = 'coar-notify:ReviewAction'
+TYPE_ENDORSEMENT = 'coar-notify:EndorsementAction'
+REVIEW_TYPES = [TYPE_REVIEW, TYPE_ENDORSEMENT]
