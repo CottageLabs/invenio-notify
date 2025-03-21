c44eaf0a714723119eef49f65643c1067f090705
seabird64 <seabird64@email.not.exit.io>
2025-03-21 09:40:38 +0000

    remove outdated copyright notices and update mappings for OpenSearch and Elasticsearch

--- invenio_notify/records/mappings/__init__.py
+++ invenio_notify/records/mappings/__init__.py
@@ -1,9 +1,1 @@
-# -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-# Copyright (C) 2016-2021 CERN.
-#
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
-
 """Mappings."""
