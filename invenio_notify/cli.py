bbed2b9081a62e5f8fd513ea89cc1b31e9f8f2fe
seabird64 <seabird64@email.not.exit.io>
2025-03-31 12:21:23 +0100

    add user management commands to notify CLI

--- invenio_notify/cli.py
+++ invenio_notify/cli.py
@@ -2,13 +2,19 @@ import click
 import json
 import rich
 from flask.cli import with_appcontext
+from invenio_accounts.models import User
+from invenio_db import db
 from sqlalchemy import desc
 
 from invenio_notify import tasks
-from invenio_notify.records.models import EndorsementMetadataModel, NotifyInboxModel
+from invenio_notify.records.models import EndorsementMetadataModel, NotifyInboxModel, ReviewerMapModel
 
 
-@click.group(chain=True)
+def print_key_value(k, v):
+    print(f'{k:<20}: {v}')
+
+
+@click.group(chain=False)
 def notify():
     """Notify commands."""
 
@@ -35,6 +41,8 @@ def get_sorted_records(model_class, size=None, order_field=None):
 @click.option('--size', '-s', type=int, default=1000, help='Maximum number of records to display')
 @with_appcontext
 def list_notify(size):
+    """ List latest endorsement and notify inbox records """
+
     console = rich.get_console()
     console.print('** Endorsement: **', style='bold')
     for r in get_sorted_records(EndorsementMetadataModel, size, order_field=desc('created')):
@@ -62,5 +70,57 @@ def list_notify(size):
         print()
 
 
-def print_key_value(k, v):
-    print(f'{k:<20}: {v}')
+@notify.group()
+def user():
+    """User commands"""
+
+
+@user.command()
+@click.option('-u', '--user', type=str, help='query by user email')
+@click.option('-r', '--reviewer_id', type=str, help='query by reviewer id')
+@with_appcontext
+def list(user, reviewer_id):
+    """ List user and reviewer id mapping """
+    if user:
+        rows = (ReviewerMapModel.query
+                .join(User, ReviewerMapModel.user_id == User.id)
+                .filter(User.email == user)
+                .all())
+    elif reviewer_id:
+        rows = (ReviewerMapModel.query
+                .filter(ReviewerMapModel.reviewer_id == reviewer_id)
+                .all())
+    else:
+        print('Please provide either email or reviewer_id to query.')
+        return
+
+    print('List of users and reviewer ids:')
+    for r in rows:
+        print(f'{r.user.email:<40} -> {r.reviewer_id}')
+
+
+@user.command()
+@click.argument('email')
+@click.argument('reviewer_id')
+@with_appcontext
+def add(email, reviewer_id):
+    """ assign coarnotify role and reviewer id to user """
+    from invenio_notify.utils import user_utils
+
+    print(f'Assign reviewer_id[{reviewer_id}] to user[{email}]')
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        print(f'User with email {email} not found.')
+        return
+
+    user_utils.add_user_action(db, user.id)
+
+    if ReviewerMapModel.query.filter_by(user_id=user.id, reviewer_id=reviewer_id).scalar():
+        print(f'User {user.email} already has a reviewer ID ({reviewer_id}) assigned.')
+        return
+
+    ReviewerMapModel.create({
+        'user_id': user.id,
+        'reviewer_id': reviewer_id,
+    })
+    db.session.commit()
