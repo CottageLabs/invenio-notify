05a6e2216cb8a407b11eef47234bad62a0f92f98
philip <philip.k@cottagelabs.com>
2025-05-20 10:38:28 +0100

    refactor: clean up imports and remove unused components

--- invenio_notify/assets/semantic-ui/js/invenio_notify/src/administration/reviewer/index.js
+++ invenio_notify/assets/semantic-ui/js/invenio_notify/src/administration/reviewer/index.js
@@ -1,12 +1,8 @@
 import { initDefaultSearchComponents } from "@js/invenio_administration";
 import { createSearchAppInit } from "@js/invenio_search_ui";
-// import { NotificationController, SearchResultItemLayout } from "@js/invenio_administration";
 import { NotificationController} from "@js/invenio_administration";
 import { SearchResultItemLayout } from "./search/SearchResultItemLayout";
-import { UserSearchLayout } from "./search";
-import { PgUserSearchLayout } from "./search";
 import { SearchFacets } from "@js/invenio_administration";
-// import { SearchBulkActionContext } from "./SearchBulkActionContext";
 
 const domContainer = document.getElementById("invenio-search-config");
 
@@ -16,7 +12,6 @@ const defaultComponents = initDefaultSearchComponents(domContainer);
 const overridenComponents = {
   ...defaultComponents,
   "InvenioAdministration.SearchResultItem.layout": SearchResultItemLayout,
-//   "SearchApp.layout": PgUserSearchLayout,
 };
 
 createSearchAppInit(
@@ -25,5 +20,4 @@ createSearchAppInit(
   "invenio-search-config",
   false,
   NotificationController
-//     SearchBulkActionContext,
 );
