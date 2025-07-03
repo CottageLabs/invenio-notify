22f9b47028b370f1003e6d43a528b2a8d3b83c2b
philip <philip.k@cottagelabs.com>
2025-07-03 11:03:41 +0100

    add inbox fixture to conftest.py

--- invenio_notify_test/conftest.py
+++ invenio_notify_test/conftest.py
@@ -10,6 +10,7 @@ from invenio_vocabularies.proxies import current_service as vocabulary_service
 from invenio_vocabularies.records.api import Vocabulary
 
 from invenio_notify_test.fixtures.endorsement_request_fixture import *  # noqa
+from invenio_notify_test.fixtures.inbox_fixture import *  # noqa
 from invenio_notify_test.fixtures.reviewer_fixture import *  # noqa
 from invenio_rdm_records.proxies import current_rdm_records
 from invenio_rdm_records.records import RDMParent, RDMRecord
